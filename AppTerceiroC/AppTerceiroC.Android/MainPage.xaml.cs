2a58cde5fbe3c9108f22580bfa4c2fd778a6e751
pbeatrizgil <beatriz.gil.pa@gmail.com>
2023-10-23 10:31:36 -0300

    salvo

---
+++ AppTerceiroC/AppTerceiroC.Android/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTerceiroC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Clicked(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(Conexao.strConexao);
+            MySqlCommand inserirNome = new MySqlCommand(ComandoSQL.inserirNome, conn);
+
+            inserirNome.Parameters.AddWithValue("@ID_ALUNO_CPF", txtNome.Text);
+            conn.Open();
+            inserirNome.ExecuteNonQuery();
+            conn.Close();
+            txtNome.Text = "";
+
+        }
+    }
+}
--- AppTerceiroC/AppTerceiroC/ComandoSQL.cs
+++ AppTerceiroC/AppTerceiroC/ComandoSQL.cs
@@ -6,6 +6,6 @@ namespace AppTerceiroC
 {
     public static class ComandoSQL
     {
-        public static string inserirNome = "INSERT INTO tbl_usuario(ID_ALUNO_CPF) VALUES (@ID_ALUNO_CPF)";
+        public static string inserirNome = "INSERT INTO `tbl_usuario`(`ID_ALUNO_CPF`, `NOME_DO_ALUNO`, `DATA_DE_NASCIMENTO`, `CONTATO_DE_EMERGENCIA`, `NOME_DO_RESPONSAVEL`, `ENDERECO1`, `ENDERECO2`,  `SERIE`) VALUES ('@ID_ALUNO_CPF','@NOME_DO_ALUNO','@DATA_DE_NASCIMENTO','@CONTATO_DE_EMERGENCIA','@NOME_DO_RESPONSAVEL','@ENDERECO1','@ENDERECO2','@SERIE')";
     }
 }
